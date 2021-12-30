52e3a20fc7c3806700e5aea27afc21ce82006b14
Lucas <lhbelfanti@gmail.com>
2021-12-30 17:06:33 -0300

    Day 25 completed.

--- src/day25/day25.go
+++ src/day25/day25.go
@@ -5,23 +5,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 type (
 	Day25 struct{}
+
+	Grid [][]string
 )
 
 func (d Day25) Part1() {
-	lines := readFile()
-	fmt.Printf("The answer is: %d\n", len(lines))
+	grid := readFile()
+
+	var count int
+	for ok := true; ok; grid, ok = step(grid, 1) {
+		count++
+	}
+
+	fmt.Printf("The answer is: %d\n", count)
 }
 
 func (d Day25) Part2() {
-	lines := readFile()
-	fmt.Printf("The answer is: %d\n", len(lines))
+	fmt.Printf("The answer is: Click on the link")
 }
 
-func readFile() []string {
+func readFile() Grid {
 	file, err := os.Open("src/day25/input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -34,5 +42,81 @@ func readFile() []string {
 		data = append(data, scanner.Text())
 	}
 
-	return data
+	var grid = make(Grid, len(data))
+	for y, row := range data {
+		grid[y] = make([]string, len(row))
+		for x, ch := range row {
+			if ch != '.' {
+				grid[y][x] = string(ch)
+			}
+		}
+	}
+
+	return grid
+}
+
+func step(g Grid, n int) (next Grid, moved bool) {
+	var m bool
+	next = make(Grid, len(g))
+	for y := range g {
+		next[y] = make([]string, len(g[y]))
+	}
+
+	for y, row := range g {
+		for x, ch := range row {
+			switch ch {
+			case "":
+				continue
+			case "v":
+				if n == 1 {
+					next[y][x] = "v"
+				} else {
+					idx := (y + 1) % len(g)
+					if g[idx][x] == "" {
+						next[idx][x] = "v"
+						moved = true
+					} else {
+						next[y][x] = "v"
+					}
+				}
+			case ">":
+				if n == 1 {
+					idx := (x + 1) % len(row)
+					if g[y][idx] == "" {
+						next[y][idx] = ">"
+						moved = true
+					} else {
+						next[y][x] = ">"
+					}
+				} else {
+					next[y][x] = ">"
+				}
+			}
+		}
+	}
+
+	if n == 1 {
+		next, m = step(next, 2)
+		if !moved {
+			moved = m
+		}
+	}
+
+	return next, moved
+}
+
+func (g Grid) String() string {
+	var sb strings.Builder
+	for _, row := range g {
+		for _, ch := range row {
+			if ch == "" {
+				sb.WriteByte('.')
+			} else {
+				sb.WriteString(ch)
+			}
+		}
+		sb.WriteString("\n")
+	}
+
+	return sb.String()
 }
