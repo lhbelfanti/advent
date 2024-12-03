45c2e5207e013128f90c20b8ac2f39ee631df978
Lucas Belfanti <lhbelfanti@gmail.com>
2024-12-03 00:28:17 -0300

    Day 2 Part 2 ✅

--- src/day2/day2.go
+++ src/day2/day2.go
@@ -35,28 +35,8 @@ func (d Day2) Part1() {
 
 	count := 0
 	for _, level := range levels {
-		count++
-		prev := 0
-		for i := 0; i < len(level)-1; i++ {
-			value := level[i] - level[i+1]
-			if abs(value) < 1 || abs(value) > 3 {
-				count--
-				break
-			}
-
-			if i > 0 {
-				if prev > 0 && value < 0 {
-					count--
-					break
-				}
-
-				if prev < 0 && value > 0 {
-					count--
-					break
-				}
-			}
-
-			prev = value
+		if isLevelSafe(level) {
+			count++
 		}
 	}
 
@@ -78,13 +58,29 @@ func (d Day2) Part2() {
 
 	levels := make([][]int, 0)
 
-	i := 0
+	n := 0
 	for scanner.Scan() {
 		elements := strings.Split(scanner.Text(), " ")
-		levels = append(levels, make([]int, 0))
+		levels = append(levels, make([]int, 0, len(elements)))
 		for _, element := range elements {
 			intElement, _ := strconv.Atoi(element)
-			levels[i] = append(levels[i], intElement)
+			levels[n] = append(levels[n], intElement)
+		}
+		n++
+	}
+
+	count := 0
+	for _, level := range levels {
+		if isLevelSafe(level) {
+			count++
+		} else {
+			for i := 0; i < len(level); i++ {
+				newLevel := removeIndex(level, i)
+				if isLevelSafe(newLevel) {
+					count++
+					break
+				}
+			}
 		}
 	}
 
@@ -92,7 +88,31 @@ func (d Day2) Part2() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The answer is: %d\n", 0)
+	fmt.Printf("The answer is: %d\n", count)
+}
+
+func isLevelSafe(level []int) bool {
+	prev := 0
+	for i := 0; i < len(level)-1; i++ {
+		value := level[i] - level[i+1]
+		if abs(value) < 1 || abs(value) > 3 {
+			return false
+		}
+
+		if i > 0 {
+			if prev > 0 && value < 0 {
+				return false
+			}
+
+			if prev < 0 && value > 0 {
+				return false
+			}
+		}
+
+		prev = value
+	}
+
+	return true
 }
 
 func abs(x int) int {
@@ -100,4 +120,10 @@ func abs(x int) int {
 		return -x
 	}
 	return x
+}
+
+func removeIndex(s []int, index int) []int {
+	ret := make([]int, 0, len(s)-1)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
 }
