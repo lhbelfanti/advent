fb46e5c9d07f6062f5e17caa9eb5e9ecf1fd0a94
Lucas Belfanti <lhbelfanti@gmail.com>
2024-12-08 01:07:26 -0300

    Day 6 Part 1 ✅

--- src/day6/day6.go
+++ src/day6/day6.go
@@ -3,6 +3,8 @@ package day6
 import (
 	"fmt"
 	"log"
+	"slices"
+	"strings"
 
 	"advent2024/src/reader"
 )
@@ -13,15 +15,95 @@ func (d Day6) Part1() {
 	file, scanner := reader.Read("src/day6/input.txt")
 	defer file.Close()
 
-	for scanner.Scan() {
+	var i, guardY, guardX int
+	maze := make([][]string, 0, 130)
 
+	for scanner.Scan() {
+		tiles := strings.Split(scanner.Text(), "")
+		idx := slices.Index(tiles, "^")
+		if idx != -1 {
+			guardY = i
+			guardX = idx
+		}
+
+		maze = append(maze, tiles)
+		i++
+	}
+
+	hasGuardLeftTheMaze := false
+	for !hasGuardLeftTheMaze {
+		//printMaze(maze)
+		guardDirection := maze[guardY][guardX]
+		switch guardDirection {
+		case "^":
+			if guardY == 0 {
+				hasGuardLeftTheMaze = true
+				maze[guardY][guardX] = "X"
+				break
+			}
+
+			if maze[guardY-1][guardX] == "#" {
+				maze[guardY][guardX] = ">"
+			} else {
+				maze[guardY-1][guardX], maze[guardY][guardX] = guardDirection, "X"
+				guardY -= 1
+			}
+		case ">":
+			if guardX == len(maze)-1 {
+				hasGuardLeftTheMaze = true
+				maze[guardY][guardX] = "X"
+				break
+			}
+
+			if maze[guardY][guardX+1] == "#" {
+				maze[guardY][guardX] = "v"
+			} else {
+				maze[guardY][guardX+1], maze[guardY][guardX] = guardDirection, "X"
+				guardX += 1
+			}
+		case "v":
+			if guardY == len(maze)-1 {
+				hasGuardLeftTheMaze = true
+				maze[guardY][guardX] = "X"
+				break
+			}
+
+			if maze[guardY+1][guardX] == "#" {
+				maze[guardY][guardX] = "<"
+			} else {
+				maze[guardY+1][guardX], maze[guardY][guardX] = guardDirection, "X"
+				guardY += 1
+			}
+		case "<":
+			if guardX == 0 {
+				hasGuardLeftTheMaze = true
+				maze[guardY][guardX] = "X"
+				break
+			}
+
+			if maze[guardY][guardX-1] == "#" {
+				maze[guardY][guardX] = "^"
+			} else {
+				maze[guardY][guardX-1], maze[guardY][guardX] = guardDirection, "X"
+				guardX -= 1
+			}
+		}
+	}
+
+	var sum int
+	for y := range maze {
+		for x := range maze[y] {
+			if maze[y][x] == "X" {
+				sum++
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The answer is: %d\n", 0)
+	fmt.Printf("The answer is: %d\n", sum)
 }
 
 func (d Day6) Part2() {
@@ -37,4 +119,15 @@ func (d Day6) Part2() {
 	}
 
 	fmt.Printf("The answer is: %d\n", 0)
+}
+
+func printMaze(maze [][]string) {
+	fmt.Println()
+	for y := range maze {
+		for x := range maze[y] {
+			fmt.Print(maze[y][x])
+		}
+		fmt.Println()
+	}
+	fmt.Println()
 }
